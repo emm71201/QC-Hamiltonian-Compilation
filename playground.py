d22050469dba4cad3f52c75c0e6dced1770c1c2a
Edison Murairi <106283492+emm71201@users.noreply.github.com>
2023-11-03 16:12:38 -0400

    updated playground.py

--- playground.py
+++ playground.py
@@ -1,12 +1,28 @@
+import numpy as np
 import helpers
 from qiskit import QuantumCircuit
 
-# qc = helpers.load_qpycircuit("RESULTS_or/QC.qpy")
-# print(qc[0])
+# reading the main circuit
+QC = helpers.load_qpycircuit("RESULTS_OR/QC.qpy")
+print("The number of gates in the main circuit: ", QC.count_ops())
 
-qc1 = helpers.load_qpycircuit("RESULTS_or/Cluster_0/time_evolution_circuit.qpy")
-print(qc1)
+# read the paulis in the first cluster
+cluster1 = np.load("RESULTS_OR/Cluster_0/commuting_pauli_strings.npy", allow_pickle=True)
+for pauli in cluster1:
+    print(pauli)
 
-print("diagonalizing circuit")
-qc1 = helpers.load_qpycircuit("RESULTS_or/Cluster_0/diagonalizing_circuit.qpy")
-print(qc1)
+# show the tableau after the pauli strings have been diagonalized
+diagonalized_tableau = np.load("RESULTS_OR/Cluster_0/diagonalized_pauli_strings_tableau.npy", allow_pickle=True).item()
+X = diagonalized_tableau["X"]
+Z = diagonalized_tableau["Z"]
+S = diagonalized_tableau["S"]
+Coefs = diagonalized_tableau["Coefs"] # values of model coefficients.
+print("The X matrix: should be all zero")
+if X.any():
+    print("The X matrix is not zero.")
+else:
+    print("And indeed it is")
+print("The Z matrix:")
+print(Z)
+
+
